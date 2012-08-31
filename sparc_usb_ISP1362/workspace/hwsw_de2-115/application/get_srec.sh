6a06a8c8ed9c0af399ff0225d54fb518006b07c4
bif <ssimhandl@ecslab6.ecs.tuwien.ac.at>
2012-08-31 18:57:46 +0200

    adapt workaround for home

--- sparc_usb_ISP1362/workspace/hwsw_de2-115/application/get_srec.sh
+++ sparc_usb_ISP1362/workspace/hwsw_de2-115/application/get_srec.sh
@@ -2,6 +2,6 @@
 
 rm main*
 
-scp -r ssimhandl@ssh.tilab.tuwien.ac.at:~/SPARC-ISP1362/sparc_usb_ISP1362/workspace/hwsw_de2-115/testapp /usr/tmp > /usr/tmp/tmp
+scp -r ssimhandl@ssh.ecs.tuwien.ac.at:~/SPARC-ISP1362/sparc_usb_ISP1362/workspace/hwsw_de2-115/application /usr/tmp > /usr/tmp/tmp
 
 mv /usr/tmp/testapp/main* ./
