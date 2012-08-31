778341b5e4449772ac338472b4b98c8919e01db3
bif <bif@new-host-2.home>
2012-08-31 19:02:45 +0200

    adapt workaround for home

--- sparc_usb_ISP1362/workspace/hwsw_de2-115/application/get_srec.sh
+++ sparc_usb_ISP1362/workspace/hwsw_de2-115/application/get_srec.sh
@@ -4,4 +4,4 @@ rm main*
 
 scp -r ssimhandl@ssh.ecs.tuwien.ac.at:~/SPARC-ISP1362/sparc_usb_ISP1362/workspace/hwsw_de2-115/application /usr/tmp > /usr/tmp/tmp
 
-mv /usr/tmp/testapp/main* ./
+mv /usr/tmp/application/main* ./
